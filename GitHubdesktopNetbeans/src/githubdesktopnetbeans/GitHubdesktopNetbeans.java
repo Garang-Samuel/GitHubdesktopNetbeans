ae1e7554d3c056b6b08d2a2e6ba780062517f318
Garang-Samuel <113675952+Garang-Samuel@users.noreply.github.com>
2022-09-30 13:43:07 +0300

    initial commit

---
+++ GitHubdesktopNetbeans/src/githubdesktopnetbeans/GitHubdesktopNetbeans.java
@@ -0,0 +1,22 @@
+
+package githubdesktopnetbeans;
+
+public class GitHubdesktopNetbeans 
+{
+
+    public static void main(String[] args)
+    {
+       
+        int a = 5000;
+        int b = 24959;
+        int c = 24777;//number c//
+        
+        int d = a * b;//addition//
+        int e = d + c;
+        
+        System.out.println("The Final Addition Answer is: "+ e);
+        
+        
+    }
+    
+}
